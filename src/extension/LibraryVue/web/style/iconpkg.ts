f9fad94c9b6a5eb2c4066d488300da4421557cdc
yArna <yArna@me.moonvy.com>
2026-07-05 22:56:07 +0800

    feat: add @haoduo-icon/lucide dependency and update workspace color settings

--- src/extension/LibraryVue/web/style/iconpkg.ts
+++ src/extension/LibraryVue/web/style/iconpkg.ts
@@ -1,4 +1,7 @@
 import "@haoduo-icon/ri"
-console.log("[iconpkg] RI icons loaded")
+console.log("[iconpkg] @haoduo-icon/ri loaded")
+import "@haoduo-icon/lucide"
+console.log("[iconpkg] @haoduo-icon/lucide loaded")
+
 // import "@haoduo-icon/fluent"
 // import "@haoduo-icon/material-symbols"
